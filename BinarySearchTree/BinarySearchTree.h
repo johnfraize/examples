629707a598bb64abbea5986dc75540438d6d4386
John Fraize <john_fraize@bose.com>
2021-09-21 20:32:54 -0400

    fixed formatting

--- BinarySearchTree/BinarySearchTree.h
+++ BinarySearchTree/BinarySearchTree.h
@@ -1,36 +1,52 @@
 namespace BST{
 
-    enum{
-	Inorder,  // left,root, right
-	Preorder, // root,left,right
-	Postorder // left,right,root
+    typedef enum{
+         Inorder,  // left,root, right
+         Preorder, // root,left,right
+         Postorder // left,right,root
     }TraversalType;
 
     template <class T>
+
+    
     class Node{
     public:
-	Node* left;
-	Node* right;	
-	T data;
+        Node* left;
+        Node* right;	
+        T data;
     };
+
+
+    
 
     template <class T>
     class Tree{
-	Node<T>* root;
+        Node<T>* root;
 	
     public:
-	Tree(){
-	    root = 0;
-	}
-	void insert(T v){
-	    if( root == 0 ){
-		root = new Node<T>;
-	    }
-	}
+        Tree(){
+            root = 0;
+        }
 
-	
-	Node<T>* walk(Node<T>* base, TraversalType tt){
+        // I think I need to provide some kind of comparison operation
+
+        
+        void insert(T v){
+            if( root == 0 ){
+                root = new Node<T>;
+            }
+            else {
+
+            }
+        }
+
+        static bool lessThan(T a, T b){
+            return false;
+        }
+
+
+        Node<T>* walk(Node<T>* base, TraversalType tt){
 	    
-	}
+        }
     };
 }
