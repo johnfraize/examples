baefb40d387a1562a638fdcf170db1f70825c243
johnfraize <johnfraize@gmail.com>
2022-07-14 05:09:25 -0400

    added latest bst in C

--- BinarySearchTree/bst.h
+++ BinarySearchTree/bst.h
@@ -1,12 +1,13 @@
 struct Node {
-  struct Node* left;
-  struct Node* right;
-  int value;
+    struct Node* left;
+    struct Node* right;
+    int data;
+    int duplicates; // how many copies of this value so if there are two values duplicates would be one
 };
 
 
 struct Node* newNode(int val);
-void addNode(struct Node* root, struct* branch);
+void addNode(struct Node* root, struct Node* branch);
 void walkFoward(struct Node* tree);
 void walkBackward(struct Node* tree);
 
