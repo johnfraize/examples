c61037b3f47df43269527b90229a4a4e30be006d
John Fraize <johnfraize@gmail.com>
2026-06-15 17:03:55 -0400

    format

---
+++ baseten.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+// to return the remainder o
+//
+// 
+// where in the number 345 , 5 is in column 0
+int dataFromDigitBase10(const int input, const int col){
+
+  // mask out the other digits my doing a modulo
+
+  // remove the upper colums
+  int phaseA = 
+  
+  int r = input % col;
+  
+  
+     int v =  (input % factor * 10) / factor;
+  
+
+}
+
+
+
+
+
+
+int main(int argc, char* argv[]){
+
+  // base 2 stuff
+  double a = pow(2,4);
+
+  double b  =  pow(10,0);
+
+  printf("stuff %f,%f\n",a,b); 
+
+  return 0;
+}
